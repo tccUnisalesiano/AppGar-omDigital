5d5fcb8ed8c1c01f4fd4ca643495812c4ca0201b
Guilherme Correia <61031607+guilhermebcorreia@users.noreply.github.com>
2022-10-04 22:08:43 -0300

    nova view carrinho

--- App.js
+++ App.js
@@ -1,27 +1,9 @@
 import React from 'react';
-import { StatusBar, SafeAreaView, View } from 'react-native';
-import { 
-  useFonts, 
-  Montserrat_400Regular, 
-  Montserrat_700Bold,
-} from '@expo-google-fonts/montserrat';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
 
-import Cesta from './src/view/cesta';
+import Carrinho from './src/telas/Carrinho';
 
 export default function App() {
-  const [fonteCarregada] = useFonts({
-    "MontserratRegular": Montserrat_400Regular,
-    "MontserratBold": Montserrat_700Bold,
-  });
-
-  if (!fonteCarregada) {
-    return <View />
-  }
-
-  return (
-    <SafeAreaView>
-      <StatusBar />
-      <Cesta />
-    </SafeAreaView>
-  );
+  return <Carrinho />;
 }
---
+++ src/telas/Servicos/index.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import { FlatList } from 'react-native';
+import Item from './Item';
+import TelaPadrao from '../../componentes/TelaPadrao';
+
+const servicos = [
+  {
+    id: 1,
+    nome: "Double Burguer",
+    preco: 19.9,
+    descricao: "Pao da casa, 2 hambúrgueres artesanais de costela (200g) e muito queijo ."
+  },
+  {
+    id: 2,
+    nome: "Double Burguer Bacon",
+    preco: 29.9,
+    descricao: "Pao da casa, 2 hambúrgueres artesanais de costela (200g), muito queijo e muito bacon."
+  },
+  {
+    id: 3,
+    nome: "BBQ Burguer",
+    preco: 25.9,
+    descricao: "Pao da casa, 2 hambúrgueres artesanais de costela suína (200g), acompanhado de molho barbecue e muito queijo ."
+  },
+  {
+    id: 4,
+    nome: "Pizza - Brócolis com Bacon",
+    preco: 79.9,
+    descricao: "Bordas com catupiry, bacon, alho frito, brócolis e cream cheese."
+  },
+  {
+    id: 5,
+    nome: "Pizza - Frango c/ Catupiry",
+    preco: 69.9,
+    descricao: "Bordas com cheddar, frango com catupiry e cream cheese."
+  },
+  {
+    id: 6,
+    nome: "Pizza - Costelinha",
+    preco: 75.9,
+    descricao: "Bordas com catupiry, costelinha suína, alho frito e cream cheese."
+  },
+  {
+    id: 7,
+    nome: "Sorvete Cascão",
+    preco: 10.9,
+    descricao: "Sabores: chocolate, creme e morango."
+  },
+  {
+    id: 8,
+    nome: "Sorvete com Churros",
+    preco: 16.9,
+    descricao: "Sabores: chocolate, creme e morango. Churros: Doce de Leite e Brigadeiro."
+  },
+  {
+    id: 9,
+    nome: "Churros Gourmet",
+    preco: 29.9,
+    descricao: "Sabores: ninho c/ nutella, brigadeiro 50% cacau."
+  }
+];
+
+export default function Servicos() {
+  return <TelaPadrao>
+    <FlatList
+      data={servicos}
+      renderItem={({item}) => <Item {...item}  />}
+      keyExtractor={({id}) => String(id)}
+    />
+  </TelaPadrao>
+}
